ef7b00d2f157378b869a00d1134a7f2696d2d488
Stephan van Stekelenburg <stephan@hayman.io>
2025-04-03 16:20:46 +0200

    feat: handle files without extension

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{
     io::Read,
     path::{Path, PathBuf},
 };
-use tree_sitter::Node;
+use tree_sitter::{Language, Node};
 
 #[derive(Parser)]
 struct Args {
@@ -22,7 +22,7 @@ fn main() {
     }
 
     println!(
-        "Scanning sources in path: {:?}",
+        "Scanning for source code in path: {:?}",
         source_folder.canonicalize().unwrap()
     );
 
@@ -32,8 +32,7 @@ fn main() {
 fn visit_dirs(path: &Path) -> std::io::Result<()> {
     if path.is_dir() {
         for entry in fs::read_dir(path)? {
-            let entry = entry?;
-            let path = entry.path();
+            let path = entry?.path();
 
             if path.is_dir() {
                 visit_dirs(&path)?;
@@ -56,29 +55,30 @@ fn visit_dirs(path: &Path) -> std::io::Result<()> {
 
 fn analyze_file(path: &Path) -> std::io::Result<()> {
     let mut parser = tree_sitter::Parser::new();
-    let file_type = path.extension().and_then(|ext| ext.to_str()).unwrap();
-    let language = get_tree_sitter_language_by_file_type(file_type)?;
+    let file_type = path.extension().and_then(|ext| ext.to_str());
 
-    parser
-        .set_language(&language)
-        .expect("Failed to set language");
+    if file_type == None {
+        return Ok(());
+    }
 
-    analyze_source_file(path.to_path_buf(), &mut parser)?;
+    if let Some(language) = tree_sitter_language_by_file_type(file_type.unwrap()) {
+        parser
+            .set_language(&language)
+            .expect("Failed to set language");
+
+        analyze_source_file(path.to_path_buf(), &mut parser)?;
+    }
+
     Ok(())
 }
 
-fn get_tree_sitter_language_by_file_type(
-    file_type: &str,
-) -> Result<tree_sitter::Language, std::io::Error> {
+fn tree_sitter_language_by_file_type(file_type: &str) -> Option<Language> {
     match file_type {
-        "rs" => Ok(tree_sitter_rust::LANGUAGE.into()),
-        "ts" => Ok(tree_sitter_typescript::LANGUAGE_TYPESCRIPT.into()),
-        "tsx" => Ok(tree_sitter_typescript::LANGUAGE_TSX.into()),
-        "cs" => Ok(tree_sitter_c_sharp::LANGUAGE.into()),
-        _ => Err(std::io::Error::new(
-            std::io::ErrorKind::Unsupported,
-            "Unsupported file type",
-        )),
+        "rs" => Some(tree_sitter_rust::LANGUAGE.into()),
+        "ts" => Some(tree_sitter_typescript::LANGUAGE_TYPESCRIPT.into()),
+        "tsx" => Some(tree_sitter_typescript::LANGUAGE_TSX.into()),
+        "cs" => Some(tree_sitter_c_sharp::LANGUAGE.into()),
+        _ => None,
     }
 }
 
@@ -93,7 +93,7 @@ fn analyze_source_file(
     let tree = parser.parse(&source_code, None).unwrap();
     let root_node = tree.root_node();
 
-    println!("File: {:?}", file_path);
+    eprintln!("File: {:?}", file_path.to_string_lossy().bold());
 
     visit_node(&source_code, root_node, 0);
     Ok(())
